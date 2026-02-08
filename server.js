bf6a0c04db97b5fbc36664f3df14ecb6f7dee73b
leviwof <ganeshkemoap123@gmail.com>
2026-02-08 19:39:49 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from '../backend/src/config/db.js';
-import userRoutes from '../backend/src/routes/userRoutes.js';
+import connectDB from './src/config/db.js';
+import userRoutes from './src/routes/userRoutes.js';
 
 dotenv.config();
 
